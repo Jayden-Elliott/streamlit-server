2855ae87c67aad86202e14a5d415c68edef24873
Jayden Elliott <jayden.elliott@basf.com>
2023-06-16 11:53:00 -0500

    Rewrote streamlit_manager.py to not use sockets for communicating to app managers.

--- streamlit_manager.py
+++ streamlit_manager.py
@@ -5,9 +5,9 @@ import threading
 import subprocess
 import pathlib
 import sys
+import time
 
 DIR = pathlib.Path(__file__).parent.absolute()
-STREAMLIT_LOG_DIR = DIR / pathlib.Path("streamlit_logs/")
 
 
 def send_tcp(msg, socket_path):
@@ -25,110 +25,102 @@ def check_port(port):
         return sock.connect_ex(("localhost", port)) == 0
 
 
-class App:
-    def start(self):
-        if check_port(self.port):
-            print(f"{self.name} could not start. \
-                  Port {self.port} already in use.\n")
-            send_tcp(
-                f"{self.name} could not start. \
-                    Port {self.port} already in use.\n",
-                self.controller_socket_path)
-            return False
-        venv_bin = pathlib.Path(self.venv) / pathlib.Path("bin")
-        command = [f"{venv_bin}/python",
-                   f"{venv_bin}/streamlit", "run", self.path]
-        command += ["--server.port", str(self.port)]
-        streamlit_log = open(f"{STREAMLIT_LOG_DIR / self.name}.log", "a")
-        self.pid = subprocess.Popen(
-            command, stdout=streamlit_log, stderr=streamlit_log).pid
-        print(f"{self.name} started with PID {self.pid}")
-        send_tcp(
-            json.dumps(
-                {"message_type": "pid_update", "name": self.name,
-                 "pid": self.pid}),
-            self.manager_socket_path)
-        return True
-
-    def main_loop(self):
-        try:
-            os.unlink(self.socket_path)
-        except OSError:
-            if os.path.exists(self.socket_path):
-                raise
-
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-            sock.bind(self.socket_path)
-            sock.listen()
-            sock.settimeout(5)
-            while True:
-                pids = subprocess.check_output(
-                    ["pgrep", "python"]).splitlines()
-                pids = [int(pid) for pid in pids]
-                if self.pid not in pids:
-                    self.start()
-
-                try:
-                    client, addr = sock.accept()
-                    data = client.recv(4096)
-                except socket.timeout:
-                    continue
-
-                try:
-                    msg = json.loads(data)
-                except json.decoder.JSONDecodeError:
-                    continue
-
-                if msg is None or "message_type" not in msg:
-                    continue
-
-                try:
-                    if msg["message_type"] == "stop":
-                        if self.pid is not None:
-                            os.kill(self.pid, 9)
-                            self.pid = None
-                        print(f"{self.name} stopped")
-                        return
-                except Exception as e:
-                    print(e)
-
-    def __init__(self, name, info,
-                 manager_socket_path, controller_socket_path):
-        self.name = name
-        self.path = info["path"]
-        self.venv = info["venv"]
-        self.port = info["port"]
-        self.socket_path = f"/tmp/streamlit-{self.name}.sock"
-        self.manager_socket_path = manager_socket_path
-        self.controller_socket_path = controller_socket_path
-        self.pid = None
-
-        if not self.start():
-            return
-        self.main_loop()
-
-
 class Manager:
-    def start_app(self, name, info):
-        self.apps[name] = info
-        app_thread = threading.Thread(target=App, args=(
-            name, info, self.socket_path, self.controller_socket_path))
-        app_thread.start()
-        self.apps[name]["thread"] = app_thread
-        self.apps[name]["socket"] = f"/tmp/streamlit-{name}.sock"
+    def manage_app(self, name):
+        if not os.path.exists(DIR / pathlib.Path("streamlit_logs")):
+            os.mkdir(DIR / pathlib.Path("streamlit_logs"))
+        streamlit_log = open(
+            DIR / pathlib.Path(f"streamlit_logs/{name}.log"), "a")
+        if check_port(self.apps[name]["port"]):
+            with streamlit_log as f:
+                f.write(
+                    f"{name} could not start. Port {self.apps[name]['port']} already in use.\n")
+            send_tcp(
+                f"{name} could not start. Port {self.apps[name]['port']} already in use.\n",
+                self.controller_socket_path)
+            return
+        venv_bin = pathlib.Path(self.apps[name]["venv"]) / pathlib.Path("bin")
+        command = [f"{venv_bin}/python", f"{venv_bin}/streamlit", "run",
+                   self.apps[name]["path"]]
+        command += ["--server.port", str(self.apps[name]["port"])]
+        app_dir = pathlib.Path(self.apps[name]["path"]).parent
+        self.apps[name]["pid"] = subprocess.Popen(
+            command, stdout=streamlit_log, stderr=streamlit_log, cwd=app_dir).pid
+        print(f"{name} started with PID {self.apps[name]['pid']}")
+        send_tcp(
+            f"{name} started on port {self.apps[name]['port']} with PID {self.apps[name]['pid']}.\n",
+            self.controller_socket_path)
+
+        while not self.apps[name]["stopped"]:
+            pids = subprocess.check_output(["pgrep", "python"]).splitlines()
+            pids = [int(pid) for pid in pids]
+            if self.apps[name]["pid"] not in pids:
+                self.apps[name]["pid"] = subprocess.Popen(
+                    command, stdout=streamlit_log, stderr=streamlit_log).pid
+                print(f"{name} restarted with PID {self.apps[name]['pid']}")
+            time.sleep(1)
+
+        if self.apps[name]["pid"] is not None:
+            os.kill(self.apps[name]["pid"], 9)
+            self.apps[name]["pid"] = None
+        print(f"{name} stopped")
+
+    def start_app(self, name):
         self.apps[name]["pid"] = None
+        self.apps[name]["stopped"] = False
+        self.apps[name]["thread"] = threading.Thread(
+            target=self.manage_app, args=(name,))
+        self.apps[name]["thread"].start()
 
     def start(self):
-        with open(DIR / pathlib.Path("apps.json")) as f:
+        with open(DIR / pathlib.Path("apps.json"), "r") as f:
             starting_apps = json.load(f)
 
         for name, info in starting_apps.items():
-            self.start_app(name, info)
+            self.apps[name] = info
+            self.start_app(name)
 
     def stop(self):
-        for info in self.apps.values():
-            send_tcp(json.dumps({"message_type": "stop"}), info["socket"])
-            info["thread"].join()
+        for vals in self.apps.values():
+            vals["stopped"] = True
+            if vals["pid"] is not None:
+                os.kill(vals["pid"], 9)
+            if vals["thread"].is_alive():
+                vals["thread"].join()
+
+    def restart_app(self, name):
+        self.apps[name]["stopped"] = True
+        self.apps[name]["thread"].join()
+        time.sleep(0.5)
+        self.start_app(name)
+
+    def refresh(self):
+        with open("apps.json") as f:
+            new_apps = json.load(f)
+            for name, info in new_apps.items():
+                if name not in self.apps:
+                    self.apps[name] = info
+                    self.start_app(name)
+                elif self.apps[name]["path"] == info["path"] \
+                        and self.apps[name]["venv"] == info["venv"] \
+                        and self.apps[name]["port"] == info["port"]:
+                    continue
+                else:
+                    self.apps[name] = info
+                    self.restart_app(name)
+
+        for name, vals in self.apps.items():
+            if not vals["thread"].is_alive():
+                self.start_app(name)
+
+    def status(self):
+        app_status = {name: {"pid": info["pid"],
+                             "port": info["port"]} for name,
+                      info in self.apps.items()}
+        send_tcp(
+            json.dumps(
+                {"message_type": "status", "apps": app_status}),
+            self.controller_socket_path)
 
     def main_loop(self):
         try:
@@ -162,39 +154,13 @@ class Manager:
 
                 elif msg["message_type"] == "refresh":
                     print("Refreshing")
-                    with open("apps.json") as f:
-                        new_apps = json.load(f)
-                    for name, info in new_apps.items():
-                        if name not in self.apps:
-                            self.start_app(name, info)
-                        elif self.apps[name]["path"] == info["path"] \
-                                and self.apps[name]["venv"] == info["venv"] \
-                                and self.apps[name]["port"] == info["port"]:
-                            continue
-                        else:
-                            send_tcp(
-                                json.dumps({"message_type": "stop"}),
-                                self.apps[name]["socket"])
-                            self.apps[name]["thread"].join()
-                            self.start_app(name, info)
+                    self.refresh()
 
                 elif msg["message_type"] == "status":
-                    pids = {name: info["pid"]
-                            for name, info in self.apps.items()}
-                    send_tcp(
-                        json.dumps(
-                            {"message_type": "status", "pids": pids}),
-                        msg["socket"])
+                    self.status()
 
-                elif msg["message_type"] == "pid_update":
-                    if msg["name"] not in self.apps:
-                        continue
-                    self.apps[msg["name"]]["pid"] = msg["pid"]
-
-                elif msg["message_type"] == "app_stopped":
-                    if msg["name"] not in self.apps:
-                        break
-                    self.apps.pop(msg["name"])
+                elif msg["message_type"] == "restart":
+                    self.restart_app(msg["app"])
 
     def __init__(self, socket_path, controller_socket_path):
         self.socket_path = socket_path
