764f44f930fef40a21dbdfbcbbd0ebb041cbf4d4
Jayden Elliott <jayden.elliott@basf.com>
2023-06-21 08:07:36 -0500

    Commented streamlit_controller.py

--- streamlit_controls/streamlit_controller.py
+++ streamlit_controls/streamlit_controller.py
@@ -31,12 +31,16 @@ def get_response(timeout):
     return response
 
 
+# Get next open port starting from START_PORT and not in used_ports
 def get_open_port(used_ports=[]):
+    # Check if port is open
     def is_open(port):
         if port in used_ports:
             return False
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             return sock.connect_ex(("localhost", port)) != 0
+
+    # Find open port
     start = START_PORT
     while not is_open(start):
         start += 1
@@ -45,19 +49,22 @@ def get_open_port(used_ports=[]):
 
 # Update config file with new apps and update app config files with correct port and url path
 def update_config():
+    # Create config file if it doesn't exist
     if not os.path.exists(DIR / Path("config.json")):
         os.system(f"touch {DIR / Path('config.json')}")
-
     config = json.load(open(DIR / Path("config.json"), "r"))
 
+    # Create apps object in config file if it doesn't exist
     if "apps" not in config:
         config["apps"] = {}
     apps = config["apps"]
     if not os.path.exists(DIR / Path("apps/")):
         os.mkdir(DIR / Path("apps/"))
 
+    # Get ports of existing apps in config.json
+    used_ports = [app["port"] for app in apps.values()]
+
     # Add new apps in apps directory to config file with default values
-    used_ports = [info["port"] for info in apps.values()]
     for app in os.listdir(DIR / Path("apps/")):
         if app in apps:
             continue
@@ -80,23 +87,42 @@ def update_config():
             "restart_on_crash": True
         }
 
+    # Write changes to config.json
     config["apps"] = apps
     json.dump(config, open(DIR / Path("config.json"), "w"), indent=4)
 
 
-# Update config file with new apps and start manager
-def start():
-    update_config()
-
-    if subprocess.call(
+# Check if manager is running
+def manager_running(exit_if_running=False):
+    running = subprocess.call(
         ["pgrep", "-f", "streamlit_manager.py"],
-            stdout=subprocess.DEVNULL) == 0:
+        stdout=subprocess.DEVNULL) == 0
+
+    if exit_if_running and running:
         print("Manager already running")
         exit(1)
+
+    if not exit_if_running and not running:
+        print("Manager not running")
+        exit(1)
+
+# Update config.json with new apps and start manager
+
+
+def start():
+    # Update config.son with new apps
+    update_config()
+
+    # Check if manager is running
+    manager_running(exit_if_running=True)
+
+    # Create log file if it doesn't exist
     if not os.path.exists(CONTROLLER_LOG_PATH):
         open(CONTROLLER_LOG_PATH, "w").close()
         os.chmod(CONTROLLER_LOG_PATH, 0o777)
     log = open(CONTROLLER_LOG_PATH, "a")
+
+    # Start manager process
     pid = subprocess.Popen(
         ["python", "-u",
          (DIR / Path("streamlit_controls/streamlit_manager.py")).as_posix(),
@@ -110,24 +136,23 @@ def start():
 
 # Stop manager and all running apps
 def stop():
-    if subprocess.call(
-        ["pgrep", "-f", "streamlit_manager.py"],
-            stdout=subprocess.DEVNULL) != 0:
-        print("Manager not running")
-        exit(1)
+    # Check if manager is running
+    manager_running()
+
+    # Send stop message to manager
     send_tcp(json.dumps({"message_type": "stop"}), MANAGER_SOCKET_PORT)
     exit(0)
 
 
 # Get status of manager and all running apps
 def status():
-    if subprocess.call(
-        ["pgrep", "-f", "streamlit_manager.py"],
-            stdout=subprocess.DEVNULL) != 0:
-        print("Manager not running")
-        exit(1)
+    # Check if manager is running
+    manager_running()
 
+    # Read status from status.json
     status = json.load(open(DIR / Path("streamlit_controls/status.json"), "r"))
+
+    # Print status
     longest_name = max([len(name) for name in status])
     print(f"{'Name': <{longest_name}}    PID")
     for name, vals in status.items():
@@ -139,14 +164,13 @@ def status():
 
 # Refresh manager with changes to config file
 def refresh():
+    # Update config.json with new apps
     update_config()
 
-    if subprocess.call(
-        ["pgrep", "-f", "streamlit_manager.py"],
-            stdout=subprocess.DEVNULL) != 0:
-        print("Manager not running")
-        exit(1)
+    # Check if manager is running
+    manager_running()
 
+    # Send refresh message to manager
     send_tcp(json.dumps({"message_type": "refresh"}),
              MANAGER_SOCKET_PORT)
     print(get_response(2), end="")
@@ -155,10 +179,15 @@ def refresh():
 
 # Restart a specific app
 def restart(app_name):
+    # Check if app name argument was provided
     if app_name is None:
         print("App name required for restart")
         exit(1)
 
+    # Check if manager is running
+    manager_running()
+
+    # Send restart message to manager
     send_tcp(
         json.dumps({"message_type": "restart", "app": app_name}),
         MANAGER_SOCKET_PORT)
@@ -166,7 +195,17 @@ def restart(app_name):
     exit(0)
 
 
+# Stop a specific app
 def stop_app(app_name):
+    # Check if app name argument was provided
+    if app_name is None:
+        print("App name required for stop")
+        exit(1)
+
+    # Check if manager is running
+    manager_running()
+
+    # Send stop message to manager
     send_tcp(
         json.dumps({"message_type": "stop_app", "app": app_name}),
         MANAGER_SOCKET_PORT)
